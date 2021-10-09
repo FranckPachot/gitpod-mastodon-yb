ef9ed986e97126fb82de6774ec3867ad4a01c111
黒ヰ樹 <3456416-tkithrta@users.noreply.gitlab.com>
2021-10-09 00:14:43 +0000

    アップデート migrate.sh

--- migrate.sh
+++ migrate.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 echo -ne "LOCAL_DOMAIN=$(gp url 8080 | cut -d/ -f3)\n$(tail -n +2 .env.production)\n" > .env.production
+echo -ne "$(head -n14 .env.production)\nSMTP_FROM_ADDRESS='Mastodon <notifications@$(gp url 8080 | cut -d/ -f3)>\n$(tail -n4 .env.production)\n" > .env.production
